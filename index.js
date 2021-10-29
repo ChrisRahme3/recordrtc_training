54038fc2833a9340a7914d34daf2ecd9d5de948c
ChrisRahme <chris.rahme@hotmail.com>
2021-10-29 09:13:07 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+import recordrtc from 'recordrtc'
+
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(async function(stream) {
+    let recorder = RecordRTC(stream, {
+        type: 'video'
+    });
+    recorder.startRecording();
+
+    const sleep = m => new Promise(r => setTimeout(r, m));
+    await sleep(3000);
+
+    recorder.stopRecording(function() {
+        let blob = recorder.getBlob();
+        invokeSaveAsDialog(blob);
+    });
+});
